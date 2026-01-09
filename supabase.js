ffeb7878758b1d453a20793b0d97c0794ea33f7a
parthpawar2424-crypto <parthpawar2424@gmail.com>
2026-01-09 20:34:21 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,16 @@
+/ supabase.js
+
+const SUPABASE_URL = "https://lnjkpbbjzjrpuwhmbqkd.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuamtwYnBpempycHVod21icWtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2MTQwNTMsImV4cCI6MjA4MzE5MDA1M30.VSPdTEwoSs4DEreHGwKSHjnDE9qGD-Lp9iLM6V3zMlw";
+
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      storage: window.localStorage
+    }
+  }
+);
